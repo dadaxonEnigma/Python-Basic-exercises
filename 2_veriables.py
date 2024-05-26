20018303716316cf7c224e28f076e6ac4e78b4a6
dadaxonEnigma <voiceofenigma19@gmail.com>
2024-05-27 02:58:29 +0300

    Finish Variavles Exercise

---
+++ 2_veriables.py
@@ -0,0 +1,13 @@
+#Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+my_year = 2001
+now_year = 2024
+my_age = now_year - my_year
+print(my_age)
+
+#Store your first, middle and last name in three different variables and then print your full name using these variables
+name = 'Dadaxon'
+surname = 'Turgunboev'
+father_name = 'Dilshodjon ugli'
+print(f'{surname} {name} {father_name}')
+
+#fdfsdf
