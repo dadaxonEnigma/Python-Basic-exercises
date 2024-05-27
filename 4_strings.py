ec72a325b13d382d32c54e8e75228d8e44e81830
dadaxonEnigma <voiceofenigma19@gmail.com>
2024-05-28 00:31:02 +0300

    4_strings Exercises

---
+++ 4_strings.py
@@ -0,0 +1,19 @@
+# Create 3 variables to store street, city and country, now create address variable to store entire address. Use two ways of creating this variable, one using + operator and the other using f-string. Now Print the address in such a way that the street, city and country prints in a separate line
+# street = 'Azizov'
+# city = 'Ferghana'
+# country = 'Uzbekistan'
+# first_adress = street + '\n' + city + '\n' + country
+# second_adress = f'{street} {city} {country}'
+# print(first_adress)
+# print(second_adress)
+
+#Create a variable to store the string "Earth revolves around the sun"
+# first_string = "Earth revolves around the sun"
+# revolve = first_string[6:14]
+# sun = first_string[-3:]
+# print(revolve)
+# print(sun)
+
+# fruits = 5
+# vegetables = 2
+# print(f"I eat {vegetables} veggies and {fruits} fruits daily")
