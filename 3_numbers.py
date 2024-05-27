33b5b5b42b946fe68a4f0795153ad322081866c9
dadaxonEnigma <voiceofenigma19@gmail.com>
2024-05-27 03:13:37 +0300

    Finish 3_numbers Exrecises

--- 3_numbers.py
+++ 3_numbers.py
@@ -1,1 +1,26 @@
-print('sdfsdf')
+# 1. You have a football field that is 92 meter long and 48.8 meter wide. Find out total
+#    area using python and print it
+# length = 92
+# width = 48.8
+# square = round(length * width,2)
+# print(square)
+
+# 2. You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar
+#    and you gave shopkeeper 20 dollar.
+#    Find out using python, how many dollars is the shopkeeper going to give you back?
+# potatoes = 1.49
+# ret_money = 20 - (9 * potatoes)
+# print(ret_money) 
+
+# 3. You want to replace tiles in your bathroom which is exactly square and 5.5 feet
+#    is its length. If tiles cost 500 rs per square feet, how much will be the total
+#    cost to replace all tiles. Calculate and print the cost using python
+#    Hint: Use power operator (**) to find area of a square
+# length = 5.5
+# area = length**2
+# cost = area * 500
+# print(cost)
+
+# 4. Print binary representation of number 17
+# num = 17
+# print('Binary of number 17 is:', format(num,'b'))
